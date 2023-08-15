eb1c9637166797d22164e2734d49b9b15e56bd7c
Bruno Francisco <bruno.francisco@codotto.com>
2023-08-15 14:34:16 +0100

    - added tests for `LogEventEventTest`

--- tests/src/Events/LogEventTest.php
+++ tests/src/Events/LogEventTest.php
@@ -1,17 +1,28 @@
 <?php
 
-use Devqaly\DevqalyClient\Events\DatabaseTransactionEvent;
+use Devqaly\DevqalyClient\Events\LogEvent;
 
-it('should throw an error when passing empty sql', function () {
+it('should throw an error when passing empty `level`', function () {
     $backendUrl = 'https://devqaly.test/api';
     $sourceIdentifier = 'microservice-x';
     $sessionId = 'c7622e14-21f8-40c2-b151-3a311816b423';
     $sessionSecret = 'c7622e14-21f8-40c2-b151-3a311816b423';
 
-    $client = new DatabaseTransactionEvent($backendUrl, $sourceIdentifier);
+    $client = new LogEvent($backendUrl, $sourceIdentifier);
 
-    $client->create($sessionId, $sessionSecret, []);
-})->throws(\Error::class, '`sql` must be set to create a database transaction event in $data');
+    $client->create($sessionId, $sessionSecret, ['log' => 'some log']);
+})->throws(\Error::class, '`level` must be set to create a log event in $data');
+
+it('should throw an error when passing empty `log`', function () {
+    $backendUrl = 'https://devqaly.test/api';
+    $sourceIdentifier = 'microservice-x';
+    $sessionId = 'c7622e14-21f8-40c2-b151-3a311816b423';
+    $sessionSecret = 'c7622e14-21f8-40c2-b151-3a311816b423';
+
+    $client = new LogEvent($backendUrl, $sourceIdentifier);
+
+    $client->create($sessionId, $sessionSecret, ['level' => LogEvent::LOG_LEVEL_ALERT]);
+})->throws(\Error::class, '`log` must be set to create a log event in $data');
 
 it('should execute curl request when calling `create` method and close', function() {
     $backendUrl = 'https://devqaly.test/api';
@@ -19,11 +30,11 @@ it('should execute curl request when calling `create` method and close', functio
     $sessionId = 'c7622e14-21f8-40c2-b151-3a311816b423';
     $sessionSecret = 'c7622e14-21f8-40c2-b151-3a311816b423';
 
-    $client = Mockery::mock(DatabaseTransactionEvent::class, [$backendUrl, $sourceIdentifier])->makePartial();
+    $client = Mockery::mock(LogEvent::class, [$backendUrl, $sourceIdentifier])->makePartial();
 
     $endpoint = 'https://something.com';
 
-    $baseData = ['sql' => 'select * from users'];
+    $baseData = ['level' => LogEvent::LOG_LEVEL_ALERT, 'log' => 'something'];
 
     $client->shouldReceive('validateSessionId')->with($sessionId)->once();
     $client->shouldReceive('validateSessionSecret')->with($sessionSecret)->once();
